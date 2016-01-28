3e0df115e161ea669dad20cb8c4b69d8fcc93a2b
evanwchen <evanwchen@gmail.com>
2016-01-27 17:17:04 -0800

    Hardcode embed a video into the app componenent

--- client/components/App.js
+++ client/components/App.js
@@ -2,7 +2,7 @@ var App = () => (
   <div> 
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer/>
+      <VideoPlayer video={window.exampleVideoData[0].id.videoId}/>
     </div>
     <div className="col-md-5">
       <VideoList/>
